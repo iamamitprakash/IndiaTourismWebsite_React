729aaf7cd3f8e56859de136aaf9886f9d8f8d648
Amit Prakash <prakashamit37@gmail.com>
2025-02-26 16:46:11 +0530

    Footer created for India Tourism Website

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { BrowserRouter as Router } from "react-router-dom";
 import Navbar from "./Components/Navbar";
+import Footer from "./Components/Footer";
 
 function App() {
   return(
     <Router>
       <Navbar />
+      <Footer />
     </Router>
   )
 }
